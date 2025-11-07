2ac20dcd0991a2e43fd2048ff0da563f88d1eb85
Gabriel James <gabbyjames@yahoo.com>
2025-11-07 02:29:26 +0100

    Added Header, MainContent, and Footer components and updated App.jsx

--- alx-react-app-props/src/components/Footer.jsx
+++ alx-react-app-props/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+    <footer>
       <p>© 2023 City Lovers</p>
     </footer>
   );
