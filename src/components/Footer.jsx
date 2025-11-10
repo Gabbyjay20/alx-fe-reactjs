d03974bc619f79a95590883e8687a37043e8de23
Gabriel James <gabbyjames@yahoo.com>
2025-11-10 02:37:04 +0100

    Moved React app to root level for checker detection

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#333',
+      color: 'white',
+      textAlign: 'center',
+      padding: '20px',
+      marginTop: 'auto'
+    }}>
+      <p>&copy; 2024 Company Name. All rights reserved.</p>
+      <p>Contact: info@company.com | Phone: (123) 456-7890</p>
+    </footer>
+  );
+}
+
+export default Footer;
