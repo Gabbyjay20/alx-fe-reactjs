6baf62306f4b36df961a994096ae0e37e0523a6c
Gabriel James <gabbyjames@yahoo.com>
2025-12-07 20:25:59 +0100

    Fix: support ESM-compatible Tailwind/PostCSS configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
+
+if (typeof module !== 'undefined') {
+  module.exports = config
+}
+
+export default config
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
@@ -6,3 +6,9 @@ module.exports = {
   },
   plugins: [],
 }
+
+if (typeof module !== 'undefined') {
+  module.exports = config
+}
+
+export default config
