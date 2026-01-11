5af26c7d3edc9e8decae153cec1fa9792c398255
Gabriel James <gabbyjames@yahoo.com>
2026-01-11 15:57:08 +0100

    Revert config files to CommonJS format for ALX checker

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
   ],
+  darkMode: false,
   theme: {
     extend: {},
   },
